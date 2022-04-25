d0855dec649a59389375ca951b6f9a1b0e803d86
IgorAntonov88 <iantonov291@gmail.com>
2022-04-25 11:06:42 +0300

    First commit

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,23 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    public static void getConnection() {
+        String url = "jdbc:mysql://localhost:3306/world?serverTimezone=Europe/Moscow&useSSL=false";
+        String username = "root";
+        String password = "8J98kZ8AB67t";
+        System.out.println("Connecting...");
+
+        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+            System.out.println("Connection successful!");
+        } catch (
+                SQLException e) {
+            System.out.println("Connection failed!");
+            e.printStackTrace();
+        }
+    }
 }
+
